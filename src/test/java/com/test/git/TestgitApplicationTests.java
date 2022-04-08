0dc2212ad08653b6ecc422303ae59c696b17f964
cocomozila <joseyun94@naver.com>
2022-04-08 16:29:24 +0900

    first commit

---
+++ src/test/java/com/test/git/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
